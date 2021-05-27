a25fe92aaad1c3dca0fae33dd3eb8bcd73224609
Regan-Mclachlan <reganmc96@hotmail.com>
2021-05-27 15:59:24 +1000

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,sex, color, temperament, alive) VALUES('Mr. Chocolate', 'M', 'Black', 'Angry', 'Yes');
+INSERT INTO bears(name,sex, color, temperament, alive) VALUES('Rowdy', 'M', 'Black', 'Angry', 'Yes');
+INSERT INTO bears(name,sex, color, temperament, alive) VALUES('Tabitha', 'M', 'Black', 'Angry', 'Yes');
+INSERT INTO bears(name,sex, color, temperament, alive) VALUES('Sergeant Brown', 'M', 'Black', 'Angry', 'Yes');
+INSERT INTO bears(name,sex, color, temperament, alive) VALUES('Melissa', 'F', 'Black', 'Angry', 'Yes');
+INSERT INTO bears(name,sex, color, temperament, alive) VALUES('Grinch', 'M', 'Black', 'Angry', 'Yes');
+INSERT INTO bears(name,sex, color, temperament, alive) VALUES('Wendy', 'M', 'Black', 'Angry', 'Yes');
+INSERT INTO bears(sex, color, temperament, alive) VALUES('M', 'Black', 'Angry', 'Yes');
